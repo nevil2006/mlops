af2769a049bdb5398c76fbc91a5c3c34eba8d9e3
NEVIL <147979766+nevil2006@users.noreply.github.com>
2026-04-17 04:48:05 +0530

    Fix line endings for ipynb files

--- fastapi/app.py
+++ fastapi/app.py
@@ -6,3 +6,9 @@ def home():
 @app.get("/hello")
 def hello():
     return {"Hello": "FastAPI"}
+@app.get("/items/{item_id}")
+def get_item(item_id: int):
+    return {"item_id": item_id}
+@app.get("/search")
+def search(name: str = None, q: str = None):
+    return {"name":name, "q":q}
